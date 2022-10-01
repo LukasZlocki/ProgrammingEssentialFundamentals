aa34c348b1047774448b42ad0c7ed4bab8f5335c
lzlocki <lzlocki@poczta.fm>
2022-10-01 16:42:53 +0200

    Types of errors

---
+++ ThreeTypesOfErrors/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Compilation error - appear during programing and is reported by ex visual studio , lack of ;
+//var Number = 5
+
+// Run time errors : accure when programs are running
+// Solution: exceptions can handle with this type of errors
+//var list = new List<int>();
+//var firstElem = list.First();
+
+// logical errors - example : all words are merged without space, logic error made by coder
+// Solution : Build unit tests for application
+string MergeWords(params string[] words)
+{
+    return string.Join("", words);
+}
+
+var sentence = MergeWords("A", "little", "Duck");
+Console.WriteLine(sentence);
+
+
+
+Console.ReadLine();
