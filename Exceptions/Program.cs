816197e9833907660fc83439ed7fc33a3327953f
lzlocki <lzlocki@poczta.fm>
2022-10-01 17:09:08 +0200

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+static int? Divide(int a, int b)
+{
+    try
+    {
+        return a / b;
+    }
+    catch(DivideByZeroException)
+    {
+        Console.WriteLine("No dividing by zero!");
+        return null;
+    }
+    finally
+    {
+        Console.WriteLine("Executing finally block");
+    }
+}
+*/
+static int? Divide(int a, int b)
+{
+    try
+    {
+        return a / b;
+    }
+    catch 
+    {
+        Console.WriteLine("Sth went wrong but i do not know what.");
+        return null;
+    }
+    finally
+    {
+        Console.WriteLine("Executing finally block");
+    }
+}
+
+
+Divide(5, 0);
+
+Console.ReadLine();
