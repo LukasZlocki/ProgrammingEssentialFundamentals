52f64ea1f6966cd5a4b5a1b73b2bc8c9d071c255
lzlocki <lzlocki@poczta.fm>
2022-10-06 21:14:16 +0200

    Ternary operator

---
+++ TernaryOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int dog = 18;
+string size;
+
+/*
+if (dog > 25)
+{
+    size = "big";
+}
+else if (dog < 5)
+{
+    size = "small";
+}
+else
+{
+    size = "medium";
+}
+*/
+
+//ternary
+//size = dog > 25 ? "big" : "small"; 
+
+// nested ternary in ternary
+size = dog > 25 ? "big" : (dog < 5 ? "small" : "medium");
+
+string comment = dog > 50 ? "This is huge dog" : "standard dog"; 
+
+Console.WriteLine($"The dog is {size}");
+Console.WriteLine($"{comment}");
+Console.ReadKey();
