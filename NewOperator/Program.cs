ea7709204bdf21b498e5b4a40cacc47e3b705918
lzlocki <lzlocki@poczta.fm>
2022-10-06 17:32:54 +0200

    New key word

---
+++ NewOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var pet1 = new Pet("Dog");
+Pet pet2 = new Pet("Cat");
+
+var lazyInitializerForPet = new LazyInitializer<Pet>();
+var lazyInitializerForList = new LazyInitializer<List<int>>();
+
+
+
+public class LazyInitializer<T> where T: new()
+{
+    public T _value;
+    public T Get()
+    {
+        if (_value == null)
+        {
+            _value = new T();
+        }
+        return _value;
+    }
+}
+
+class Pet
+{
+    string Name;
+
+    public Pet(string name)
+    {
+        Name = name;
+    }
+
+}
+
