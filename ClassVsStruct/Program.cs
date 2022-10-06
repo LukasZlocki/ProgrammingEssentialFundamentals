b4444551c8d5ac2ab5f5271d3682b7f4d9a77886
lzlocki <lzlocki@poczta.fm>
2022-10-06 14:23:23 +0200

    Class Vs Struct

---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,20 @@
+﻿struct Point
+{
+    public int x;
+    public int y;
+
+    /* constructor need to have the sme amount of agruments as fields
+    public Point()
+    {
+
+    }
+    */
+
+    // constructor
+    public Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+}
