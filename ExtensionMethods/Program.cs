f2751451d2f1e9d615c8bca0d6d42f46f2b777b6
lzlocki <lzlocki@poczta.fm>
2022-10-11 21:21:24 +0200

    ExtensionMethods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿
+var multilineString = @"ASDLFJALKSDFLLKJLKSDJF,
+                        SDJFLKJSDLKFJASDLKJFLASDJ,
+                        KLJSDLKJFLSDLFJSDFLSDJFLSDJF,
+                        KJSDLKAFJLASJDFLJASDFASDFSDF,
+                        KLKJLSDFJLKSJDFOOOOOOOOOOOOO.";
+
+// extension method usage
+var numberOfLines = multilineString.GetNumberOfLines(); // <--- ! 
+Console.WriteLine($"number of lines is {numberOfLines}");
+Console.ReadLine();
+
+
+//public static class StringOperations
+//{
+//    public static int GetNumberOfLines(string input)
+//    {
+//        return input.Split("\n").Length;
+//    }
+//}
+
+
+// extension : static clas, static methos, this keyword
+public static class StringExtensions
+{
+    public static int GetNumberOfLines(this string input)
+    {
+        return input.Split("\n").Length;
+    }
+}
