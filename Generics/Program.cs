520a7212b3c9c7272a58edfb0036d44bc9b970cd
lzlocki <lzlocki@poczta.fm>
2022-10-20 21:16:18 +0200

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿// Generics
+
+
+var currencies = new Dictionary<string, string>
+{
+    ["USA"] = "USD",
+    ["Great Britain"] = "GBP"
+};
+
+
+
+// List of Types
+class List<T>
+{
+    public void Add(T item)
+    {
+        // ...
+    }
+}
+
+
+
+/*
+// Solution without Generics
+class IntegersList
+{
+    public void Add(int item)
+    {
+         // ...
+    }
+}
+class DoublesList
+{
+    public void Add(double item)
+    {
+        // ...
+    }
+}
+class StringsList
+{
+    public void Add(string item)
+    {
+        // ...
+    }
+}
+*/
