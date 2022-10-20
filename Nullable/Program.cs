0007496316a3aa8c6600546a4a7259b6c5f532f0
lzlocki <lzlocki@poczta.fm>
2022-10-20 18:32:38 +0200

    nullable types

---
+++ Nullable/Program.cs
@@ -0,0 +1,18 @@
+﻿// nullable
+
+//int Height = null // <--error
+int? height = null;
+Nullable<int> height2 = null;
+
+if (height.HasValue)
+{
+    Console.WriteLine($"Height is {height.Value}");
+}
+else
+{
+    Console.WriteLine("Height is null");
+}
+
+int number = height.Value;
+
+Console.ReadLine();
