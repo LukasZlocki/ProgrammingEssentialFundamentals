d668e08b8279aa90ebc188f5cd68e98490a85ef4
lzlocki <lzlocki@poczta.fm>
2022-10-22 09:33:01 +0200

    Overloading methods

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿// Section 34 "Overloading methods"
+
+Console.WriteLine("ints 2x: " + Mathem.Add(1, 1));
+
+Console.WriteLine("strings: " + Mathem.Add("Joe", " is me"));
+Console.WriteLine("ints 3x: " + Mathem.Add(1, 1, 4));
+
+class Mathem
+{
+    public static int Add (int a, int b)
+    {
+        return a + b;
+    }
+
+    public static string Add (string a, string b)
+    {
+        return a + b;
+    }
+
+    public static int Add (int a, int b, int c)
+    {
+        return (a + b) + c;
+    }
+}
