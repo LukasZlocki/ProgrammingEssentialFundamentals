790dbec85e1577209e4f25b1de0001cf31fafd2f
lzlocki <lzlocki@poczta.fm>
2022-10-24 22:21:33 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿// Section 41 "Singleton"
+
+public sealed class Singleton
+{
+    private static Singleton instance = null;
+    public string Id { get;} // Only for presentaion purpose
+
+    private Singleton()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
