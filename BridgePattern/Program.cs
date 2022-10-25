1ca4c3d1c3bbd3095d3ccb915a98a4a6c5d5f764
lzlocki <lzlocki@poczta.fm>
2022-10-25 21:49:48 +0200

    Bridge pattern

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,48 @@
+﻿// Section 44 "Bridge pattern"
+// The Bridge design pattern allows us to split an inheritance hierarchy into a set of hierarchies .
+// It is the implementation of the "composition over inheritance" principle
+
+var electricPickupWithManualGear = new Pickup(new ElectricMotor(), new ManualGear());
+
+var petrolSedanWithAutomaticGear = new Sedan( new PetrolMotor(), new AutomaticGear());
+
+
+
+
+class Car
+{
+    public Motor Motor { get; }
+    public Gear Gear { get; }
+
+    public Car(Motor motor, Gear gear)
+    {
+        Motor = motor;
+        Gear = gear;
+    }
+}
+
+
+class Pickup : Car
+{
+    public Pickup(Motor motor, Gear gear) : base(motor, gear)
+    {
+
+    }
+}
+
+class Sedan : Car
+{
+    public Sedan(Motor motor, Gear gear) : base(motor, gear)
+    {
+
+    }
+}
+
+
+class Motor { }
+class ElectricMotor : Motor { }
+class PetrolMotor : Motor { }
+
+class Gear { }
+class ManualGear : Gear { }
+class AutomaticGear : Gear { }
