33e91e932679406678b8ed690776226928452658
lzlocki <lzlocki@poczta.fm>
2022-10-30 08:45:47 +0100

    SOLID_D

---
+++ SOLID_D/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Section: 50 "SOLID - D - Interface Segregation Principle (ISP)"
+*/
+
+
+// Example - refactoring to meets dependency inversion pronciple
+class YourStore
+{
+    private readonly IDelivery _delivery;
+
+    public YourStore(IDelivery delivery)
+    {
+        _delivery = delivery;
+    }
+
+    public void SellItem(string item, string address)
+    {
+        var package = PreparePackage(item, address);
+        _delivery.DeliverPackage(package);
+    }
+
+    private string PreparePackage(string item, string address)
+    {
+        return "package reaty to be shipped";
+    }
+
+}
+
+public interface IDelivery
+{
+    void DeliverPackage(string package);
+}
+
+internal class FastWheelsDelivery : IDelivery
+{
+    public void DeliverPackage(string package)
+    {
+        //delivering with a bike
+    }
+}
+
+internal class HeavyCargoDelivery : IDelivery
+{
+    public void DeliverPackage(string package)
+    {
+        //delivering with TIR accross the country, even heaviest packages
+    }
+}
+
+
+
+/*
+// Example - initial situation
+class YourStore{
+    FastWheelsDelivery _delivery = new FastWheelsDelivery();
+
+    public void SellItem(string item, string address){
+        var package = PreparePackage(item, address);
+        _delivery.DeliverPackage(package);
+    }
+`
+    private string PreparePackage(string item, string address){
+        return "package reaty to be shipped";
+    }
+
+}
+
+
+internal class FastWheelsDelivery{
+    public void DeliverPackage(string package){
+        //delivering with a bike
+    }
+}
+
+*/
+
