f27a504acbea42b7ccc6f3f53417d7d5cc0ae405
lzlocki <lzlocki@poczta.fm>
2023-03-21 22:48:46 +0100

    Add menu and task descriptions

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,14 @@
 ﻿// BankAccount 
-Console.WriteLine("Hello, BankAccount!");
+// Use design pattern that suits building menu
+// Use polimorf to master objective oriented programing
+
+Console.Clear();
+Console.WriteLine("Welcome to the Bank");
+Console.WriteLine("Menu");
+Console.WriteLine("A - Balance check");
+Console.WriteLine("B - Deposit");
+Console.WriteLine("C - Withdraw");
+Console.WriteLine("");
+Console.Write(">>> ");
+string _menuOption = Console.ReadLine();
+
