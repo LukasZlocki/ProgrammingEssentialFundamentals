a062a24fd6d4de4d92d913f62518095778e2221f
lzlocki <lzlocki@poczta.fm>
2023-03-21 22:32:23 +0100

    Add account model with basic methods

---
+++ BankAccount/Models/Account.cs
@@ -0,0 +1,31 @@
+namespace BankAccount.Models
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerLastName {get; set;}
+        public double Balance { get; set; }
+
+
+        public string Withdraw(double amount) {
+            string _userMessage;
+            if (Balance - amount >= 0) {
+                Balance = Balance - amount;
+                _userMessage = "Succes. Your new account balance : " + ToString(Balance);
+            }
+            else {
+                _userMessage = "No sufficient funds.";
+            }
+            return _userMessage;
+        }
+
+        public void Deposit(double amount) {
+            // code logic here
+        }
+
+        public string CheckBalance() {
+            return "Your account balance :" + ToString(Balance);
+        }
+    }
+}
