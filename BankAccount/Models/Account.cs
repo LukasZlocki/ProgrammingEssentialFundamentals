bdb572e125b6c42e511cf2f24de4b208c0ae9276
lzlocki <lzlocki@poczta.fm>
2023-03-22 17:54:52 +0100

    Refactor code

--- BankAccount/Models/Account.cs
+++ BankAccount/Models/Account.cs
@@ -8,16 +8,14 @@ namespace BankAccount.Models
         public double Balance { get; set; }
 
 
-        public string Withdraw(double amount) {
-            string _userMessage;
+        public virtual string Withdraw(double amount) {
             if (Balance - amount >= 0) {
                 Balance = Balance - amount;
-                _userMessage = "Succes. Your new account balance : " + Balance.ToString();
+                return "Succes. Your new account balance : " + Balance.ToString();
             }
             else {
-                _userMessage = "No sufficient funds.";
+                return "No sufficient funds.";
             }
-            return _userMessage;
         }
 
         public void Deposit(double amount) {
