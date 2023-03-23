3cf8861c50e8e662104b3cf353c50bc9519bda41
lzlocki <lzlocki@poczta.fm>
2023-03-23 19:39:09 +0100

    Add menu

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -8,7 +8,28 @@ Console.WriteLine("Menu");
 Console.WriteLine("A - Balance check");
 Console.WriteLine("B - Deposit");
 Console.WriteLine("C - Withdraw");
+Console.WriteLine("D - Add bank account");
+Console.WriteLine("X - Exit");
 Console.WriteLine("");
 Console.Write(">>> ");
 string _menuOption = Console.ReadLine();
+
+switch(_menuOption) {
+    case "A":
+        // code here
+        break;
+    case "B":
+        // code here
+        break;
+    case "C":
+        // code here
+        break;
+    case "D":
+        // code here
+        break;
+    case "X":
+        // code here
+        break;
+}
+
 
